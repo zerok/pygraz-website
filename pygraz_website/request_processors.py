86d72a2b7177c3dbdde165ecf58c6db9f0cf606f
Horst Gutmann <zerok@zerokspot.com>
2010-10-02 18:35:54 +0200

    It is now possible to fake a user login for offline testing

--- pygraz_website/request_processors.py
+++ pygraz_website/request_processors.py
@@ -1,8 +1,10 @@
-from flask import session, g
+from flask import session, g, current_app
 from . import documents
 
 
 def check_user():
+    if 'FAKE_LOGIN' in current_app.config and 'openid' not in session:
+        session['openid'] = current_app.config['FAKE_LOGIN']
     if 'openid' in session:
         g.user = documents.User.view('frontend/users_by_openid',
                 key=session['openid']).first()
