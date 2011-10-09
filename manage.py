38963345a8df26e9216cac6750a97a789ef02735
Horst Gutmann <zerok@zerokspot.com>
2011-10-09 15:10:56 +0200

    More flake8 cleanup

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@ from flaskext.script import Manager
 import pygraz_website
 from pygraz_website import models, db
 import tweepy
-from os.path import join, dirname, abspath
 import settings
 
 app = pygraz_website.create_app(config_object=settings)
@@ -10,33 +9,41 @@ app = pygraz_website.create_app(config_object=settings)
 
 manager = Manager(app)
 
+
 @manager.option("--dry-run", dest='dryrun', action='store_true')
 @manager.command
 def sync_twitter(dryrun=False):
-    last_tweet = models.Tweet.query.order_by(models.Tweet.created_at.desc()).first()
+    last_tweet = models.Tweet.query\
+            .order_by(models.Tweet.created_at.desc()).first()
     last_id = None
     if last_tweet:
         print "Last ID: " + str(last_tweet.external_id)
         last_id = last_tweet.external_id
-    for tweet in tweepy.Cursor(tweepy.api.user_timeline, 'pygraz', since_id=last_id).items(100):
+    for tweet in tweepy.Cursor(tweepy.api.user_timeline, 'pygraz',
+            since_id=last_id).items(100):
         if dryrun:
             print tweet.id
         else:
             db.session.add(models.Tweet.from_tweet(tweet))
             db.session.commit()
 
+
 @manager.command
 def create_db():
     db.create_all()
+
 
 @manager.command
 def runserver():
     app.run()
 
+
 @manager.option('--make-admin', dest='make_admin', action='store')
 @manager.command
 def update_user(openid, make_admin=None):
-    user = db.session.query(models.OpenID).filter(models.OpenID.id==openid).first().user
+    user = db.session.query(models.OpenID)\
+            .filter(models.OpenID.id == openid)\
+            .first().user
     print user
     if make_admin == '1':
         user.is_admin = True
@@ -45,4 +52,6 @@ def update_user(openid, make_admin=None):
     db.session.add(user)
     db.session.commit()
 
-manager.run()
+
+if __name__ == '__main__':
+    manager.run()
